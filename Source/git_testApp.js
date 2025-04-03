d0bf3bc9f8ed064e11e873282a49841ba03b12b1
KeonHeeOh <98ogh@naver.com>
2025-04-03 14:07:12 +0900

    첫번째

---
+++ Source/git_testApp.js
@@ -0,0 +1,33 @@
+
+git_testApp = class git_testApp extends AApplication
+{
+	constructor()
+	{
+		super()
+
+		//TODO:edit here
+
+	}
+
+	onReady()
+	{
+		super.onReady();
+
+		this.setMainContainer(new APage('main'))
+		this.mainContainer.open('Source/MainView.lay')
+
+		//TODO:edit here
+
+	}
+
+	unitTest(unitUrl)
+	{
+		//TODO:edit here
+
+		this.onReady()
+
+		super.unitTest(unitUrl)
+	}
+
+}
+
